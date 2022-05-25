af7dbc3fc67207d477d3f6aea77adec155989034
guoyf <guoyf@siactpower.com>
2022-05-25 18:16:12 +0800

    1、增加 历史工作流模块、全局配置模块 枚举类型  常量类型 2、修改 历史工作流模块 全局配置模块  代码中使用的枚举类型 常量

--- src/view/configuration/visitCall/AddOrEidtDailog.vue
+++ src/view/configuration/visitCall/AddOrEidtDailog.vue
@@ -129,6 +129,8 @@ import {
   saveOrEdite, simulationRequest
 } from "@/api/globalConfig";
 
+import ApiEnum from "@/enum/ApiTypeEnum";
+
 export default {
   name: "AddOrEidtDailog",
   props: {
@@ -144,24 +146,6 @@ export default {
       jsonData: [],
       value: '',
       apiBoxList: [
-        // {
-        //   source: '', //系统名称
-        //   sourceMark: '', // 系统标识
-        //   name: '', //api名称
-        //   apiMark: '', // api标识
-        //   type: '', // api类型,
-        //   typeName: '', //api类型名称
-        //   host: '',// 系统host
-        //   path: '',// 请求路径
-        //   method: '', //请求方式
-        //   headers: '', //请求头信息
-        //   parameter: '', // GET请求参数 eg: ?id=${id}&&name=${name}
-        //   body: '', //POST请求参数 eg: {\"id\":\"${id}\",\"name\":\"${name}\"}
-        //   dataParse: '', //解析配置
-        //   isUse: '', // 是否使用 1 使用 0禁用 2删除
-        //   createTime: '', //创建时间
-        //   tenantId: '' //租户id
-        // },
         {
             ...this.guideForm,
             name: '', //api名称
@@ -240,7 +224,7 @@ export default {
     },
 
     excuteParse(api) {
-      if (api.method === 'POST') {
+      if (api.method === ApiEnum.API_TYPE_POST) {
         simulationRequest({
           "body": api.body,
           "headers": api.headers,
@@ -294,7 +278,7 @@ export default {
             pars[parse.key] =  parse.value
         })
         apibox.dataParse = JSON.stringify(pars)
-        if (apibox.method === 'POST') {
+        if (apibox.method === ApiEnum.API_TYPE_POST) {
           let obj = {}
             apibox.configParams.forEach(item=> {
               obj[item.key] = item.value
