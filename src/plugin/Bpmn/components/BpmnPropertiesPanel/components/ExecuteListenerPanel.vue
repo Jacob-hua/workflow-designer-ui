5b7f151b223e16df3f6f709f7d4a0d5d98179c0f
YanHeng <yanh@siactpower.com>
2022-05-26 18:16:19 +0800

    修复bpmn选择时属性更新方式

---
+++ src/plugin/Bpmn/components/BpmnPropertiesPanel/components/ExecuteListenerPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-table-column></el-table-column>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'executeListenerPanel',
+}
+</script>
+
+<style>
+</style>
