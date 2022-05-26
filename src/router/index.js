96fe6b12b07835383b3d44e442addba3e1ef4f7d
YanHeng <yanh@siactpower.com>
2022-05-26 14:54:18 +0800

    增加debugBpmn调试页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,92 +1,88 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import layout from'@/layout/index.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import layout from "@/layout/index.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: '/',
-    redirect: 'login',
+    path: "/",
+    name: "/",
+    redirect: "login",
     // redirect: 'home',
   },
   {
-  	path: '/login',
-    name: 'login',
-  	component: () => import('@/view/login/login'),
-  	hidden: true
+    path: "/login",
+    name: "login",
+    component: () => import("@/view/login/login"),
+    hidden: true,
   },
   {
-    path: '/home',
-    name: 'first',
+    path: "/home",
+    name: "first",
     component: layout,
-    redirect: '/home/bpmn',
+    redirect: "/home/bpmn",
     children: [
       {
-        path: '/home/bpmn',
-        name: 'bpmn',
-        component: () => import('@/view/workflow/index'),
-        meta: { title: 'bpmn', icon: 'dashboard' }
+        path: "/home/bpmn",
+        name: "bpmn",
+        component: () => import("@/view/workflow/index"),
+        meta: { title: "bpmn", icon: "dashboard" },
       },
       {
-        path: '/home/form',
-        name: 'form',
-        component: () => import('@/view/form/index'),
-        meta: { title: 'bpmn', icon: 'dashboard' }
+        path: "/home/form",
+        name: "form",
+        component: () => import("@/view/form/index"),
+        meta: { title: "bpmn", icon: "dashboard" },
       },
       {
-        path: '/home/home',
-        name: 'home',
-        component: () => import('@/view/home/index'),
-        meta: { title: 'bpmn', icon: 'dashboard' }
+        path: "/home/home",
+        name: "home",
+        component: () => import("@/view/home/index"),
+        meta: { title: "bpmn", icon: "dashboard" },
       },
       {
-        path: '/home/history',
-        name: 'history',
-        component: () => import('@/view/historyWorkflow/HistoryWorkflow'),
-        meta: { title: 'history', icon: 'dashboard' }
+        path: "/home/history",
+        name: "history",
+        component: () => import("@/view/historyWorkflow/HistoryWorkflow"),
+        meta: { title: "history", icon: "dashboard" },
       },
       {
-        path: '/home/runTime',
-        name: 'runTime',
-        component: () => import('@/view/runtime/index'),
-        hidden: true
-      },
-      {
-        path: '5',
-        name: '5',
-        component: () => import('@/layout/404'),
-        hidden: true
-      },
-      {
-        path: '/home/all',
-        name: 'all',
-        component: () => import('@/view/configuration'),
+        path: "/home/runTime",
+        name: "runTime",
+        component: () => import("@/view/runtime/index"),
         hidden: true,
       },
       {
-        path: '/home/power',
-        name: 'power',
-        component: () => import('@/view/power'),
+        path: "5",
+        name: "5",
+        component: () => import("@/layout/404"),
         hidden: true,
-      }
-      // {
-      //    path: '6-1',
-      //    name: '6-1',
-      //    component: () => import('@/views/configuration'),
-      // },
-      // {
-      //    path: '6-2',
-      //    name: '6-2',
-      //    component: () => import('@/views/404'),
-      // }
-    ]
+      },
+      {
+        path: "/home/all",
+        name: "all",
+        component: () => import("@/view/configuration"),
+        hidden: true,
+      },
+      {
+        path: "/home/power",
+        name: "power",
+        component: () => import("@/view/power"),
+        hidden: true,
+      },
+      {
+        path: "/home/debugBpmn",
+        name: "debugBpmn",
+        component: () => import("@/view/debugBpmn"),
+        meta: { title: "bpmn", icon: "dashboard" },
+      },
+    ],
   },
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/view/debugBpmn/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <bpmn-editor />
+    <bpmn-properties-panel />
+    <button @click="onSubmit">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'debugBpmn',
+}
+</script>
+
+<style>
+.wrapper {
+  width: 100%;
+  height: 90%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+}
+</style>
