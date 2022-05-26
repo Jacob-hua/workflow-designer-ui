c8d6482327b908a0e1dc7484fc0ab264efd6f36f
YanHeng <yanh@siactpower.com>
2022-05-26 18:16:19 +0800

    修复bpmn选择时属性更新方式

--- src/plugin/Bpmn/store/module/bpmn.js
+++ src/plugin/Bpmn/store/module/bpmn.js
@@ -41,6 +41,62 @@ function generateState() {
     extendProperties: [],
     configBtton: [],
     elementText: "",
+    configurationInfo: {
+      listenerType: {
+        class: {
+          label: "Java 类",
+          value: "Java 类",
+        },
+        expression: {
+          label: "表达式",
+          value: "表达式",
+        },
+        delegateExpression: {
+          label: "代理表达式",
+          value: "代理表达式",
+        },
+        script: {
+          label: "脚本",
+          value: "脚本",
+        },
+      },
+      eventType: {
+        create: {
+          label: "创建",
+          value: "创建",
+        },
+        assignment: {
+          label: "指派",
+          value: "指派",
+        },
+        complete: {
+          label: "完成",
+          value: "完成",
+        },
+        delete: {
+          label: "删除",
+          value: "删除",
+        },
+        update: {
+          label: "更新",
+          value: "更新",
+        },
+        timeout: {
+          label: "超时",
+          value: "超时",
+        },
+      },
+      fieldType: {
+        string: {
+          label: "字符串",
+          value: "字符串",
+        },
+        expression: {
+          label: "表达式",
+          value: "表达式",
+        },
+      },
+    },
   };
 }
 
@@ -68,9 +124,9 @@ const actions = {};
 export const iBpmnListener = {
   "selection.changed": ({ newSelection }, refreshBpmnState) => {
     if (Array.isArray(newSelection) && newSelection[0]) {
-      refreshBpmnState({
-        name: newSelection[0].name,
-      });
+      refreshBpmnState(newSelection[0].businessObject);
+    } else {
+      refreshBpmnState(generateState())
     }
   },
   "shape.changed": ({ element }, refreshBpmnState) => {
