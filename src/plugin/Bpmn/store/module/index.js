ba20f347a5e2485f519f345cb60e10505320846c
YanHeng <yanh@siactpower.com>
2022-05-31 15:37:27 +0800

    重构moudle/index.js

--- src/plugin/Bpmn/store/module/index.js
+++ src/plugin/Bpmn/store/module/index.js
@@ -1,20 +1,65 @@
 import { vuexNamespace } from "../../config";
 import { deepCopy } from "../../utils/object";
 
-const requireModule = require.context(".", false, /\.js$/);
-const modules = requireModule.keys().reduce((modules, modulePath) => {
-  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, "$1");
-  if (moduleName === "index") {
+function loadModules() {
+  const requireModule = require.context(".", false, /\.js$/);
+  const modules = requireModule.keys().reduce((modules, modulePath) => {
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, "$1");
+    if (moduleName === "index") {
+      return modules;
+    }
+    modules[moduleName] = requireModule(modulePath).default;
     return modules;
-  }
-  modules[moduleName] = requireModule(modulePath).default;
+  }, {});
   return modules;
-}, {});
+}
 
-// 用于记录bpmn模块的初始状态
-const initialModuleState = {};
+function checkModules(modules, moduleTemplate) {
+  Object.keys(modules).forEach((moduleName) => {
+    patchModule(modules[moduleName], moduleTemplate);
+  });
+}
 
-// 所有bpmn模块都有的mutation方法
+function patchModule(module, moduleTemplate) {
+  Object.keys(moduleTemplate)
+    .filter((property) => !module.hasOwnProperty(property))
+    .forEach((property) => {
+      module[property] = moduleTemplate[property]();
+    });
+}
+
+function cacheModuleState(modules) {
+  const cache = Object.keys(modules).reduce((cache, moduleName) => {
+    const module = modules[moduleName];
+    cache[moduleName] = deepCopy(module.state ?? {});
+    return cache;
+  }, {});
+  return cache;
+}
+
+function injectCommonMutations(modules, commonMutations) {
+  Object.keys(modules).forEach((moduleName) => {
+    const module = modules[moduleName];
+    Object.keys(commonMutations)
+      .filter((mutationKey) => !module.mutations.hasOwnProperty(mutationKey))
+      .forEach((mutationKey) => {
+        module.mutations[mutationKey] = commonMutations[mutationKey](moduleName);
+      });
+  });
+}
+
+const modules = loadModules();
+
+checkModules(modules, {
+  state: () => ({}),
+  getters: () => ({}),
+  mutations: () => ({}),
+  actions: () => ({}),
+  namespaced: () => true,
+});
+
+const initialModuleState = cacheModuleState(modules);
+
 const commonMutations = {
   refreshState() {
     return function (state, payload = {}) {
@@ -30,18 +75,6 @@ const commonMutations = {
   },
 };
 
-// 遍历所有模块，执行初始状态记录和公共mutation添加
-Object.keys(modules).forEach((moduleName) => {
-  const module = modules[moduleName];
-  initialModuleState[moduleName] = deepCopy(module.state ?? {});
-  if (!module["mutations"]) {
-    module["mutations"] = {};
-  }
-  Object.keys(commonMutations)
-    .filter((mutationKey) => !module.mutations.hasOwnProperty(mutationKey))
-    .forEach((mutationKey) => {
-      module.mutations[mutationKey] = commonMutations[mutationKey](moduleName);
-    });
-});
+injectCommonMutations(modules, commonMutations);
 
 export default modules;
