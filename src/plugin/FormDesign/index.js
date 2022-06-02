b5bdf2b3271a4cd7a5998b0195e156fe9c8c033e
guoyf <guoyf@siactpower.com>
2022-06-02 16:21:44 +0800

    test

--- src/plugin/FormDesign/index.js
+++ src/plugin/FormDesign/index.js
@@ -16,7 +16,7 @@ const plugins = {
     Vue.component('icon-dialog',iconDialog);
   }
 };
-// 这一步判断window.Vue是否存在，因为直接引用vue.min.js， 它会把Vue绑到Window上，我们直接引用打包好的js才能正常跑起来。
+ // 这一步判断window.Vue是否存在，因为直接引用vue.min.js， 它会把Vue绑到Window上，我们直接引用打包好的js才能正常跑起来。
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(plugins);
 }
