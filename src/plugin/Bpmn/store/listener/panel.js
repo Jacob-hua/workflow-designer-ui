d927cbc3759317738a3f9b9d80c165fee50d31ff
YanHeng <yanh@siactpower.com>
2022-06-06 11:31:36 +0800

    执行监听器双向映射

--- src/plugin/Bpmn/store/listener/panel.js
+++ src/plugin/Bpmn/store/listener/panel.js
@@ -1,12 +1,53 @@
-function convertListener(listener) {
-  console.log(listener);
+import IBpmn from "../../iBpmn";
+
+function selectedElementListeners(iBpmn = new IBpmn()) {
+  const listeners = iBpmn.getSelectedShapeInfoByDefaultLocalName("ExecutionListener");
+  return listeners?.map(listener2State);
+
+  function listener2State(listener) {
+    const state = {};
+    state.event = listener.event;
+    if (listener.class) {
+      state.listenerType = "class";
+      state.class = listener.class;
+    } else if (listener.expression) {
+      state.listenerType = "expression";
+      state.expression = listener.expression;
+    } else if (listener.delegateExpression) {
+      state.listenerType = "delegateExpression";
+      state.delegateExpression = listener.delegateExpression;
+    } else if (listener.script) {
+      if (listener.script.value) {
+        state.scriptType = "inline";
+        state.scriptValue = listener.script.value;
+      } else {
+        state.scriptType = "outside";
+        state.scriptResource = listener.script.resource;
+      }
+    }
+    state.fields = listener.fields?.map(listenerField2State);
+    return state;
+  }
+
+  function listenerField2State(field) {
+    const state = {};
+    state.name = field.name;
+    if (field.string) {
+      state.fieldType = "string";
+      state.value = field.string;
+    } else if (field.expression) {
+      state.fieldType = "expression";
+      state.value = field.expression;
+    }
+    return state;
+  }
 }
 
 export default {
-  "selection.changed": ({ newSelection }, commit) => {
-    if (Array.isArray(newSelection) && newSelection[0]) {
-      convertListener(newSelection[0]?.businessObject)
-      commit("refreshState", newSelection[0]?.businessObject);
+  "selection.changed": (_, commit, iBpmn) => {
+    const listeners = selectedElementListeners(iBpmn);
+    if (listeners) {
+      commit("refreshState", { listeners });
     } else {
       commit("initState");
     }
