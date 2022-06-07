826a0e43626b220fd80c2f520febf94cef87aae6
YanHeng <yanh@siactpower.com>
2022-06-07 17:06:13 +0800

    替换bpmnjsEle文件的bpmn设计器和面板

--- src/view/workflow/component/addBpmn.vue
+++ src/view/workflow/component/addBpmn.vue
@@ -1,10 +1,17 @@
 <template>
-  <el-dialog title="新建流程" :visible="dialogVisible" width="90%" custom-class="dialogVisible" @close="close">
+  <el-dialog title="新建流程"
+             :visible="dialogVisible"
+             width="90%"
+             custom-class="dialogVisible"
+             @close="close">
     <div class="dialogVisible-main">
-      <bpmnJsELe ref="bpmnJsELe" @initModeler="initModeler" :xmlString="xmlString" v-if="dialogVisible"></bpmnJsELe>
+      <bpmnJsELe :xmlString="xmlString"
+                 v-if="dialogVisible"></bpmnJsELe>
     </div>
-    <span slot="footer" class="dialog-footer">
-      <el-button type="primary" @click="publish()">发布</el-button>
+    <span slot="footer"
+          class="dialog-footer">
+      <el-button type="primary"
+                 @click="publish()">发布</el-button>
       <el-button @click="confirm()">保存</el-button>
       <el-button @click="cancel()">取消</el-button>
     </span>
@@ -14,99 +21,95 @@
 <script>
 import {
   workFlowSaveDraft,
-  workFlowSave, publishWorkflow
+  workFlowSave,
+  publishWorkflow,
 } from '@/api/managerWorkflow'
-  import bpmnJsELe from '@/view/bpmnJsELe/index.vue'
-  // import bpmnJsELe from '@/view/bnpmJs/index.vue'
-  import X2JS from "x2js";
-  import {
-    postDesignDesignService
-  } from '@/api/unit/api.js'
-  export default {
-    props: {
-      flag: {
-        type: Boolean,
-        default: false
-      },
-      formData: {
-        type: Object,
-        default: () =>{
+import bpmnJsELe from '@/view/bpmnJsELe/index.vue'
+import X2JS from 'x2js'
 
-        }
+export default {
+  components: {
+    bpmnJsELe,
+  },
+  props: {
+    flag: {
+      type: Boolean,
+      default: false,
+    },
+    formData: {
+      type: Object,
+      default: () => {},
+    },
+    dialogVisible: {
+      type: Boolean,
+      default: false,
+    },
+    publick: {
+      type: String,
+      default: '',
+    },
+    currentRowData: {
+      type: Object,
+      default: {
+        name: '',
+        id: '',
       },
-      dialogVisible: {
-        type: Boolean,
-        default: false
+    },
+    xmlString: {
+      type: String,
+      default: '',
+    },
+    pubFlag: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      modeler: null,
+      postData: {
+        name: '',
       },
-      publick: {
-        type: String,
-        default: ''
-      },
-      currentRowData: {
-        type: Object,
-        default:  {
-          name: '',
-          id: ''
-        }
-      },
-      xmlString: {
-        type: String,
-        default: ''
-      },
-      pubFlag: {
-        type: Boolean,
-        default: false
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      if (this.$refs.bpmnJsELe) {
+        this.$refs.bpmnJsELe.$refs.panel.$refs.baseInfo.elementBaseInfo.name =
+          this.formData.name
       }
-    },
-    data() {
-      return {
-        modeler: null,
-        postData: {
-          name: ''
-        }
-      }
-    },
-    mounted() {
-      this.$nextTick(() => {
-        console.log(this.formData)
-        if (this.$refs.bpmnJsELe) {
-          this.$refs.bpmnJsELe.$refs.panel.$refs.baseInfo.elementBaseInfo.name = this.formData.name
-        }
-      })
-    },
-    methods: {
-      publish() {
-        let _this = this
-        const newConvert = new X2JS();
-        this.modeler.saveXML({
-          format: true
-        }).then(({
-                   xml
-                 }) => {
-          const {
-            definitions
-          } = newConvert.xml2js(xml);
+    })
+  },
+  methods: {
+    publish() {
+      let _this = this
+      const newConvert = new X2JS()
+      this.modeler
+        .saveXML({
+          format: true,
+        })
+        .then(({ xml }) => {
+          const { definitions } = newConvert.xml2js(xml)
           var file1 = new File([xml], definitions.process._name + '.bpmn', {
-            type: 'bpmn20-xml'
-          });
-          console.log(definitions)
+            type: 'bpmn20-xml',
+          })
           let formData = new FormData()
           if (_this.currentRowData?.id) {
-            formData.append('id', _this.currentRowData.id )
+            formData.append('id', _this.currentRowData.id)
           }
           let names = ''
           if (definitions?.process._name) {
             names = definitions.process._name
           }
-          if ( _this.formData?.name) {
-            names =  _this.formData.name
+          if (_this.formData?.name) {
+            names = _this.formData.name
           }
           if (_this.currentRowData?.name) {
             names = _this.currentRowData.name
           }
 
           formData.append('name', names)
-          formData.append('docName',   names+ '.bpmn')
+          formData.append('docName', names + '.bpmn')
           if (_this.publick) {
             formData.append('ascription', 'public')
           } else {
@@ -122,49 +125,38 @@ import {
           formData.append('file', file1)
           // 已发布的 走修改的流程
           if (_this.pubFlag) {
-            formData.append('id', _this.currentRowData.id )
-            workFlowSaveDraft(formData).then((res) => {
+            formData.append('id', _this.currentRowData.id)
+            workFlowSaveDraft(formData).then(() => {
               _this.$message.success('保存成功')
-              // this.$router.push('/home')
               _this.$emit('close')
               _this.$parent.findWorkFlowRecord()
             })
-          } else  {
-            publishWorkflow(formData).then(res => {
+          } else {
+            publishWorkflow(formData).then(() => {
               this.$message.success('发布成功')
-              // this.$router.push('/home')
               this.$emit('close')
               this.$parent.findWorkFlowRecord('enabled,disabled')
             })
           }
-
-        });
-        this.$emit('confirm')
-
-      },
-
-      cancel() {
-        this.$emit('close')
-      },
-      close() {
-        this.$emit('close')
-      },
-      initModeler(value) {
-        this.modeler = value
-      },
-      confirm() {
-        console.log(this.flag)
-        let _this = this
-        let names
-        const newConvert = new X2JS();
-        this.modeler.saveXML({
-          format: true
-        }).then(({
-          xml
-        }) => {
-          const {
-            definitions
-          } = newConvert.xml2js(xml);
+        })
+      this.$emit('confirm')
+    },
+    cancel() {
+      this.$emit('close')
+    },
+    close() {
+      this.$emit('close')
+    },
+    confirm() {
+      let _this = this
+      let names
+      const newConvert = new X2JS()
+      this.modeler
+        .saveXML({
+          format: true,
+        })
+        .then(({ xml }) => {
+          const { definitions } = newConvert.xml2js(xml)
           if (definitions.process._name) {
             names = definitions.process._name
           }
@@ -175,14 +167,22 @@ import {
             names = _this.currentRowData.name
           }
           var file1 = new File([xml], names + '.bpmn', {
-            type: 'bpmn20-xml'
-          });
-          console.log(definitions)
-          console.log(_this.currentRowData)
+            type: 'bpmn20-xml',
+          })
           let formData = new FormData()
-            formData.append('id', _this.currentRowData.id )
-          formData.append('name', _this.currentRowData.name || _this.formData.name || definitions.process._name)
-          formData.append('docName',  definitions.process._name+ '.bpmn' || _this.currentRowData.name+'.bpmn'|| _this.formData.name + '.bpmn')
+          formData.append('id', _this.currentRowData.id)
+          formData.append(
+            'name',
+            _this.currentRowData.name ||
+              _this.formData.name ||
+              definitions.process._name
+          )
+          formData.append(
+            'docName',
+            definitions.process._name + '.bpmn' ||
+              _this.currentRowData.name + '.bpmn' ||
+              _this.formData.name + '.bpmn'
+          )
           if (_this.flag && Object.values(_this.formData).length > 0) {
             formData.append('ascription', _this.$parent.projectCode)
           } else {
@@ -196,37 +196,26 @@ import {
           formData.append('updateBy', this.$store.state.userInfo.name)
           formData.append('tenantId', this.$store.state.tenantId)
           formData.append('file', file1)
-          _this.flag?
-              workFlowSave(formData).then((res) => {
+          _this.flag
+            ? workFlowSave(formData).then((res) => {
                 _this.$message.success('保存成功')
-                // this.$router.push('/home')
                 _this.$emit('close')
                 _this.$parent.findWorkFlowRecord('drafted')
               })
-          :
-              workFlowSaveDraft(formData).then((res) => {
+            : workFlowSaveDraft(formData).then((res) => {
                 _this.$message.success('保存成功')
-                // this.$router.push('/home')
                 _this.$emit('close')
                 _this.$parent.findWorkFlowRecord('drafted')
               })
-
-        });
-        _this.$emit('confirm')
-      }
+        })
+      _this.$emit('confirm')
     },
-    components: {
-      bpmnJsELe
-    }
-  }
+  },
+}
 </script>
 
-<style scoped="scoped">
- /* /deep/ .el-dialog__body {
-    height: 80vh;
-  } */
-
-  .dialogVisible-main {
-    height: 80vh;
-  }
+<style scoped>
+.dialogVisible-main {
+  height: 80vh;
+}
 </style>
