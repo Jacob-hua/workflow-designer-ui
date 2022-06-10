5038f5ad09845f00b395f6da8bc1e6f1148c1cdc
zhu qingyun <zhuqy@siactpower.com>
2022-06-10 18:28:23 +0800

    clean code

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,3 +1,4 @@
+import { userLogin } from '@/constant/index.js'
 <template>
   <div class="background body" v-show="showHtml">
      <div class="box">
@@ -21,9 +22,9 @@
 </template>
 
 <script>
-
+import CONSTANT from '@/constant'
 import { userLogin } from '@/api/unit/api.js'
-// import md5 from 'js-md5'
+
 export default {
   name: 'LoginPage',
   data () {
@@ -37,7 +38,7 @@ export default {
     if(this.$route.query.account) {
       this.username = this.$route.query.account
       this.password = this.$route.query.account
-      sessionStorage.setItem('status', '我是菜鸡')
+      sessionStorage.setItem('status', CONSTANT.LOGIN_FROM_WORKFLOW_ITSELF)
       this.login()
     } else {
       sessionStorage.clear()
