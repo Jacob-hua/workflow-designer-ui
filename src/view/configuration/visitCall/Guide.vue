dc6a1c7d42fbcc34e7bec64a2d187924da931600
guoyf <guoyf@siactpower.com>
2022-06-15 11:51:35 +0800

    1、全局配置新增配置向导页 增加资源名称 资源标识 必填校验

--- src/view/configuration/visitCall/Guide.vue
+++ src/view/configuration/visitCall/Guide.vue
@@ -8,17 +8,17 @@
       append-to-body
       >
     <span>
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-form-item label="资源名称">
+        <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+          <el-form-item prop="source" label="资源名称">
             <el-input v-model="form.source"></el-input>
           </el-form-item>
-          <el-form-item label="资源标识">
+          <el-form-item prop="sourceMark" label="资源标识">
             <el-input v-model="form.sourceMark"></el-input>
           </el-form-item>
         </el-form>
     </span>
     <span slot="footer" class="dialog-footer">
-      <el-button  @click="dialogVisible = false; $emit('showAddDialog', form, editFlag)">下一步</el-button>
+      <el-button  @click=" next()">下一步</el-button>
       <el-button @click="dialogVisible = false">取 消</el-button>
 
   </span>
@@ -36,6 +36,15 @@ export default {
   },
   data() {
     return {
+      rules: {
+        source: [
+          { required: true, message: '请输入资源名称', trigger: 'blur' }
+        ],
+        sourceMark: [
+          { required: true, message: '请输入资源标识', trigger: 'blur' }
+        ],
+
+      },
       dialogVisible: false,
       form: {
         source: '',
@@ -45,7 +54,17 @@ export default {
     }
   },
    methods: {
-
+      next() {
+        this.$refs['form'].validate((valid) => {
+          if (valid) {
+            this.dialogVisible = false;
+            this.$emit('showAddDialog',this.form, this.editFlag)
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
    }
 }
 </script>
