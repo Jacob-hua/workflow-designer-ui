a8c9f542533a8f8f5ba0a58255cb054f1bc8212b
YanHeng <yanh@siactpower.com>
2022-06-15 14:59:50 +0800

    修复时间引发的添加后不更新问题

--- src/util/date.js
+++ src/util/date.js
@@ -24,10 +24,13 @@ export function currentDateRangeFormat(format = "YYYY-MM-DD") {
   return dateRangeFormat(new Date(), format);
 }
 
-export function oneMonthAgo(date, format = "YYYY-MM-DD") {
+export function oneMonthAgo(date, format = "YYYY-MM-DD", startOfDay = true) {
+  if (startOfDay) {
+    return moment(date).subtract(1, "month").startOf("day").format(format);
+  }
   return moment(date).subtract(1, "month").format(format);
 }
 
-export function currentOneMonthAgo(format = "YYYY-MM-DD") {
-  return { start: oneMonthAgo(new Date(), format), end: moment(new Date()).format(format) };
+export function currentOneMonthAgo(format = "YYYY-MM-DD", startOfDay = true) {
+  return { start: oneMonthAgo(new Date(), format, startOfDay), end: moment(new Date()).endOf("day").format(format) };
 }
