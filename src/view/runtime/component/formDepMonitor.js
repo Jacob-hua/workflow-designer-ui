26fafe0bc0460de9cad3a80665cd9f310714b019
YanHeng <yanh@siactpower.com>
2022-06-20 11:57:59 +0800

    删除没有用到的枚举

--- src/view/runtime/component/formDepMonitor.js
+++ src/view/runtime/component/formDepMonitor.js
@@ -1,4 +1,4 @@
-import { FormTypeEnum, ApiEnum } from '../../../enum'
+import { ApiEnum } from '../../../enum'
 
 export function variableFactory({ method, parameter, body }) {
   const variablesHandlers = {
