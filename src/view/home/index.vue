294b05f7ea7b3a8c38a4b272d0b10ac09bb30e0a
YanHeng <yanh@siactpower.com>
2022-06-27 16:20:32 +0800

    删除无用组件，调整文件名

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -2,39 +2,47 @@
   <div class="home">
     <div class="home-filter">
       <div class="projectSelect">
-        <el-select @change="projectChange"
-                   v-model="value1">
-          <el-option v-for="{id, label, value} in rootOrganizations" :key="id" :label="label" :value="value"></el-option>
+        <el-select @change="projectChange" v-model="ascription">
+          <el-option
+            v-for="{ id, label, value } in rootOrganizations"
+            :key="id"
+            :label="label"
+            :value="value"
+          ></el-option>
         </el-select>
       </div>
       <div class="businessSelect">
-        <el-cascader style="margin-right: 10px;"
-                     v-model="value2"
-                     :options="rootOrganizationChildrenAndAll(value1)"
-                     :props='cascaderProps'
-                     clearable
-                     @change="handleChange"></el-cascader>
+        <el-cascader
+          style="margin-right: 10px"
+          v-model="projectCode"
+          :options="rootOrganizationChildrenAndAll(ascription)"
+          :props="cascaderProps"
+          clearable
+          @change="handleChange"
+        ></el-cascader>
       </div>
       <div class="datePick">
         <span class="datePickTitle">时间</span>
-        <el-date-picker v-model="valueDate"
-                        type="daterange"
-                        align="right"
-                        unlink-panels
-                        range-separator="——"
-                        start-placeholder="开始日期"
-                        end-placeholder="结束日期"
-                        value-format="yyyy-MM-dd HH:mm:ss"
-                        :default-time="['00:00:00', '23:59:59']"
-                        :clearable="false"
-                        @change="getManyData()">
+        <el-date-picker
+          v-model="valueDate"
+          type="daterange"
+          align="right"
+          unlink-panels
+          range-separator="——"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          :default-time="['00:00:00', '23:59:59']"
+          :clearable="false"
+          @change="getManyData()"
+        >
         </el-date-picker>
       </div>
     </div>
     <div class="home-header">
       <div class="data1">
         <div>
-          <div class="title">{{ deployNumber }}</div>
+          <div class="title">{{ deployedWorkflowContents }}</div>
           <div class="titLabel">已部署工作流</div>
         </div>
         <div>
@@ -44,15 +52,15 @@
       </div>
       <div class="data2">
         <div>
-          <div class="title">{{ numberList.executionTotalProcessCount }}</div>
+          <div class="title">{{ workflowContents.executionTotalProcessCount }}</div>
           <div class="titLabel">执行工作流总数</div>
         </div>
         <div>
-          <div class="title">{{ numberList.executionInProcessCount }}</div>
+          <div class="title">{{ workflowContents.executionInProcessCount }}</div>
           <div class="titLabel">执行中</div>
         </div>
         <div>
-          <div class="title">{{ numberList.executionCompleteCount }}</div>
+          <div class="title">{{ workflowContents.executionCompleteCount }}</div>
           <div class="titLabel">已完成数量</div>
         </div>
       </div>
@@ -67,28 +75,36 @@
     </div>
     <div class="home-main">
       <div class="home-main-tab">
-        <span class="home-main-tab-item"
-              :class="activeName === 'first' ? 'active' : ''"
-              @click="changeAction('first')">工作流（{{ WorkflowTableNum }}）</span>
-        <span class="home-main-tab-item"
-              :class="activeName === 'second' ? 'active' : ''"
-              @click="changeAction('second')">草稿箱（{{ draftsTableNum }}）</span>
+        <span class="home-main-tab-item" :class="activeName === 'first' ? 'active' : ''" @click="changeAction('first')">
+          工作流（{{ WorkflowTableNum }}）
+        </span>
+        <span
+          class="home-main-tab-item"
+          :class="activeName === 'second' ? 'active' : ''"
+          @click="changeAction('second')"
+        >
+          草稿箱（{{ draftsTableNum }}）
+        </span>
       </div>
       <div class="home-table">
-        <WorkflowTable v-show="activeName === 'first'"
-                       :valueDate="valueDate"
-                       :ascription="value1"
-                       :business="value2"
-                       @totalChange="totalChange"
-                       ref="first"
-                       @getManyData="getManyData()"></WorkflowTable>
-        <draftsTable v-show="activeName === 'second'"
-                     :valueDate="valueDate"
-                     :ascription="value1"
-                     :business="value2"
-                     @totalChange="totalChange"
-                     ref="second"
-                     @getManyData="getManyData()"></draftsTable>
+        <WorkflowTable
+          v-show="activeName === 'first'"
+          :valueDate="valueDate"
+          :ascription="ascription"
+          :business="projectCode"
+          @totalChange="totalChange"
+          ref="first"
+          @getManyData="getManyData()"
+        ></WorkflowTable>
+        <draftsTable
+          v-show="activeName === 'second'"
+          :valueDate="valueDate"
+          :ascription="ascription"
+          :business="projectCode"
+          @totalChange="totalChange"
+          ref="second"
+          @getManyData="getManyData()"
+        ></draftsTable>
       </div>
     </div>
   </div>
@@ -100,7 +116,7 @@ import draftsTable from './component/draftsTable.vue'
 import { getDeployCount, getTaskCountStatistic } from '@/api/unit/api.js'
 import { getProjectList } from '@/api/globalConfig'
 import { mapState, mapGetters, mapActions } from 'vuex'
-import {currentOneMonthAgo} from '@/util/date'
+import { currentOneMonthAgo } from '@/util/date'
 
 export default {
   components: {
@@ -110,24 +126,19 @@ export default {
   data() {
     const { start, end } = currentOneMonthAgo('yyyy-MM-DD HH:mm:ss')
     return {
-      sysProps: {
-        label: 'name',
-        value: 'code',
-      },
       projectOption: [],
-      systemOption: [],
-      valueDate: [start,end],
-      numberList: {
+      valueDate: [start, end],
+      workflowContents: {
         executionCompleteCount: 0,
         executionInProcessCount: 0,
         executionTotalProcessCount: 0,
       },
       activeName: 'first',
-      value1: '',
-      value2: '',
+      ascription: '',
+      projectCode: '',
       WorkflowTableNum: 0,
       draftsTableNum: 0,
-      deployNumber: 0,
+      deployedWorkflowContents: 0,
     }
   },
   computed: {
@@ -137,8 +148,7 @@ export default {
   },
   async created() {
     await this.dispatchRefreshOrganization()
-    this.value1 = this.findRootOrganizationByIndex(0).value
-    // await this.getProjectList()
+    this.ascription = this.findRootOrganizationByIndex(0).value
     this.getManyData()
   },
   methods: {
@@ -146,24 +156,8 @@ export default {
     handleChange() {
       this.getManyData()
     },
-    deleteEmptyChildren(arr) {
-      for (let i = 0; i < arr.length; i++) {
-        const arrElement = arr[i]
-        if (!arrElement.children.length) {
-          delete arrElement.children
-          continue
-        }
-        if (arrElement.children) {
-          this.deleteEmptyChildren(arrElement.children)
-        }
-      }
-    },
     projectChange(val) {
       this.getManyData()
-      this.systemOption = this.projectOption.filter(
-        ({ code }) => code === val
-      )[0].children
-      this.deleteEmptyChildren(this.systemOption)
     },
     async getProjectList() {
       let res = await getProjectList({
@@ -173,14 +167,11 @@ export default {
         type: '',
       })
       this.projectOption = res?.result ?? []
-      this.value1 = this.projectOption[0].code
-      this.systemOption = this.projectOption[0].children
-      this.deleteEmptyChildren(this.systemOption)
-      // this.value2 = this.systemOption[0].code
+      this.ascription = this.projectOption[0].code
     },
     goBpmn() {
       let { menuProjectList } = JSON.parse(sessionStorage.getItem('loginData'))
-      
+
       this.menuList = menuProjectList.filter((item) => {
         return item.projectList.length > 0
       })
@@ -189,7 +180,7 @@ export default {
       })
       if (isWorkflowRole === -1) {
         this.$message.error('无权限')
-      } else{
+      } else {
         this.$router.push('/home/Workflow')
       }
     },
@@ -202,14 +193,14 @@ export default {
     },
     getDeployCountList() {
       getDeployCount({
-        ascription: this.value1,
-        business: this.value2.at(-1),
+        ascription: this.ascription,
+        business: this.projectCode,
         startTime: this.valueDate[0],
         endTime: this.valueDate[1],
         status: 'activation',
         tenantId: this.tenantId,
       }).then((res) => {
-        this.deployNumber = res.result
+        this.deployedWorkflowContents = res.result
       })
     },
     getManyData() {
@@ -222,17 +213,17 @@ export default {
     },
     getDataNumber() {
       getTaskCountStatistic({
-        ascription: this.value1,
+        ascription: this.ascription,
         assignee: this.userInfo.account,
-        business: this.value2.at(-1),
+        business: this.projectCode,
         endTime: this.valueDate[1],
         startTime: this.valueDate[0],
         tenantId: this.tenantId,
       }).then((res) => {
         if (res) {
-          this.numberList = res.result
+          this.workflowContents = res.result
         } else {
-          this.numberList = {
+          this.workflowContents = {
             executionCompleteCount: 0,
             executionInProcessCount: 0,
             executionTotalProcessCount: 0,
