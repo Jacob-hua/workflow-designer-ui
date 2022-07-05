56d1b3d03c09c69574ad3b23184d9c55261f6dbb
YanHeng <yanh@siactpower.com>
2022-07-05 14:58:13 +0800

    新增el-tooltip.scss

--- src/plugin/Bpmn/components/BpmnEditor/components/ControlHeader.vue
+++ src/plugin/Bpmn/components/BpmnEditor/components/ControlHeader.vue
@@ -1,86 +1,43 @@
 <template>
   <div>
-    <el-button-group>
-      <el-button icon="el-icon-folder-opened"
-                 type="primary"
-                 :size="buttonSize"
-                 @click="openFile">打开文件</el-button>
+    <div class="tool-wrapper">
+      <el-button icon="el-icon-folder-opened" type="primary" :size="buttonSize" @click="openFile">打开文件</el-button>
       <el-tooltip effect="light">
-        <el-button type="primary"
-                   icon="el-icon-download"
-                   :size="buttonSize">下载文件</el-button>
-        <div slot="content"
-             class="tooltip-content">
-          <el-button :size="buttonSize"
-                     type="text"
-                     @click="downloadAs('xml')">下载为XML文件</el-button>
-          <el-button :size="buttonSize"
-                     type="text"
-                     @click="downloadAs('svg')">下载为SVG文件</el-button>
-          <el-button :size="buttonSize"
-                     type="text"
-                     @click="downloadAs('bpmn')">下载为BPMN文件</el-button>
+        <el-button type="primary" icon="el-icon-download" :size="buttonSize">下载文件</el-button>
+        <div slot="content" class="tooltip-content">
+          <el-button :size="buttonSize" type="text" @click="downloadAs('xml')">下载为XML文件</el-button>
+          <el-button :size="buttonSize" type="text" @click="downloadAs('svg')">下载为SVG文件</el-button>
+          <el-button :size="buttonSize" type="text" @click="downloadAs('bpmn')">下载为BPMN文件</el-button>
         </div>
       </el-tooltip>
       <el-tooltip effect="light">
-        <el-button type="primary"
-                   icon="el-icon-view"
-                   :size="buttonSize">预览</el-button>
-        <div slot="content"
-             class="tooltip-content">
-          <el-button :size="buttonSize"
-                     type="text"
-                     @click="previewAs('xml')">预览XML</el-button>
+        <el-button type="primary" icon="el-icon-view" :size="buttonSize">预览</el-button>
+        <div slot="content" class="tooltip-content">
+          <el-button :size="buttonSize" type="text" @click="previewAs('xml')">预览XML</el-button>
           <br />
-          <el-button :size="buttonSize"
-                     type="text"
-                     @click="previewAs('json')">预览JSON</el-button>
+          <el-button :size="buttonSize" type="text" @click="previewAs('json')">预览JSON</el-button>
         </div>
       </el-tooltip>
-    </el-button-group>
-    <el-button-group>
-      <el-button :size="buttonSize"
-                 :disabled="defaultZoom < 0.2"
-                 icon="el-icon-zoom-out"
-                 @click="canvasZoomOut" />
-      <el-button :size="buttonSize">{{ zoomRatio }}</el-button>
-      <el-button :size="buttonSize"
-                 :disabled="defaultZoom > 4"
-                 icon="el-icon-zoom-in"
-                 @click="canvasZoomIn" />
-      <el-button :size="buttonSize"
-                 icon="el-icon-c-scale-to-original"
-                 @click="canvasResetZoom" />
-    </el-button-group>
-    <el-button-group>
-      <el-tooltip effect="light"
-                  content="撤销">
-        <el-button :size="buttonSize"
-                   :disabled="!undoable"
-                   icon="el-icon-refresh-left"
-                   @click="operationUndo" />
-      </el-tooltip>
-      <el-tooltip effect="light"
-                  content="恢复">
-        <el-button :size="buttonSize"
-                   :disabled="!redoable"
-                   icon="el-icon-refresh-right"
-                   @click="operationRedo" />
-      </el-tooltip>
-      <el-tooltip effect="light"
-                  content="重新绘制">
-        <el-button :size="buttonSize"
-                   icon="el-icon-refresh"
-                   @click="repaint" />
-      </el-tooltip>
-    </el-button-group>
-    <el-dialog title="预览"
-               width="60%"
-               :visible.sync="previewModal.visible"
-               append-to-body
-               destroy-on-close>
-      <highlightjs :language="previewModal.type"
-                   :code="previewModal.code" />
+      <el-button-group>
+        <el-button :size="buttonSize" :disabled="defaultZoom < 0.2" icon="el-icon-zoom-out" @click="canvasZoomOut" />
+        <el-button :size="buttonSize">{{ zoomRatio }}</el-button>
+        <el-button :size="buttonSize" :disabled="defaultZoom > 4" icon="el-icon-zoom-in" @click="canvasZoomIn" />
+        <el-button :size="buttonSize" icon="el-icon-c-scale-to-original" @click="canvasResetZoom" />
+      </el-button-group>
+      <el-button-group>
+        <el-tooltip effect="light" content="撤销">
+          <el-button :size="buttonSize" :disabled="!undoable" icon="el-icon-refresh-left" @click="operationUndo" />
+        </el-tooltip>
+        <el-tooltip effect="light" content="恢复">
+          <el-button :size="buttonSize" :disabled="!redoable" icon="el-icon-refresh-right" @click="operationRedo" />
+        </el-tooltip>
+        <el-tooltip effect="light" content="重新绘制">
+          <el-button :size="buttonSize" icon="el-icon-refresh" @click="repaint" />
+        </el-tooltip>
+      </el-button-group>
+    </div>
+    <el-dialog title="预览" width="60%" :visible.sync="previewModal.visible" append-to-body destroy-on-close>
+      <highlightjs :language="previewModal.type" :code="previewModal.code" />
     </el-dialog>
   </div>
 </template>
@@ -155,8 +112,7 @@ export default {
       }
     },
     canvasZoomOut() {
-      const newZoom =
-        Math.floor(this.defaultZoom * 100 - this.zoomStep * 100) / 100
+      const newZoom = Math.floor(this.defaultZoom * 100 - this.zoomStep * 100) / 100
       if (newZoom <= this.minZoom) {
         this.$message.warning('已经到最小比例')
         return
@@ -165,8 +121,7 @@ export default {
       this.$iBpmn.canvasZoom(this.defaultZoom)
     },
     canvasZoomIn() {
-      const newZoom =
-        Math.floor(this.defaultZoom * 100 + this.zoomStep * 100) / 100
+      const newZoom = Math.floor(this.defaultZoom * 100 + this.zoomStep * 100) / 100
       if (newZoom >= this.maxZoom) {
         this.$message.warning('已经到最大比例')
         return
@@ -191,7 +146,37 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.tool-wrapper {
+  display: flex;
+  flex-direction: row;
+
+  & > button {
+    margin-right: 6px;
+    margin-left: 0px;
+
+    &:last-of-type {
+      margin-right: 30px;
+      background: #0dd5ef;
+      border-color: #0dd5ef;
+    }
+  }
+
+  .el-button-group {
+    margin-right: 30px;
+
+    .el-button {
+      background: transparent;
+      border-color: $border-color;
+      color: $button-color;
+    }
+
+    .is-disabled {
+      color: $button-disabled-color;
+    }
+  }
+}
+
 .tooltip-content {
   display: flex;
   flex-direction: column;
