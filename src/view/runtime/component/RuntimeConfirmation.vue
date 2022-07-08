1c902f15dafa47490e42495495e9d8577834d6b1
YanHeng <yanh@siactpower.com>
2022-07-08 16:42:29 +0800

    对应挂起面板样式

--- src/view/runtime/component/RuntimeConfirmation.vue
+++ src/view/runtime/component/RuntimeConfirmation.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <el-dialog append-to-body :title="title" :visible="visible" @close="onCancel">
-      <div class="runtimeConfirmation">
-        <div class="title">{{ message }}</div>
+      <div>
         <el-form ref="form" :model="form" label-width="40px">
           <el-form-item label="账号">
             <el-input v-model="form.username" @keyup.native.enter="onSubmit" :disabled="true"></el-input>
@@ -12,9 +11,9 @@
           </el-form-item>
         </el-form>
       </div>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="onCancel">取 消</el-button>
-        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      <span slot="footer">
+        <el-button class="submit-button" @click="onSubmit">确 定</el-button>
+        <el-button class="cancel-button" @click="onCancel">取 消</el-button>
       </span>
     </el-dialog>
   </div>
@@ -33,10 +32,6 @@ export default {
     title: {
       type: String,
       default: '确认标题',
-    },
-    message: {
-      type: String,
-      default: '请输入用户名密码',
     },
   },
   computed: {
@@ -68,18 +63,16 @@ export default {
 }
 </script>
 
-<style scoped="scoped">
-.runtimeConfirmation {
-  padding: 0px 40px;
+<style scoped lang="scss">
+/deep/ .el-dialog {
+  @include formDialog;
 }
 
-.title {
-  text-align: center;
-  color: #ff3352;
-  margin: 20px 0px;
+.submit-button {
+  @include primaryBtn;
 }
 
-.rejectWord {
-  margin: 20px 0px;
+.cancel-button {
+  @include cancelBtn;
 }
 </style>
