aa0062d0f290db55bf988f7b20268a0a9f2fbbd0
YanHeng <yanh@siactpower.com>
2022-07-09 11:21:22 +0800

    查看工作流详情->工单详情

--- src/view/runtime/component/lookover.vue
+++ src/view/runtime/component/lookover.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-dialog title="查看流程信息" top="1vh" :visible="visible" @close="onClose">
+  <el-dialog title="工单详情" top="1vh" :visible="visible" @close="onClose">
     <div class="container">
       <ProcessInformation
         :xml="workflow.processDeployResource"
