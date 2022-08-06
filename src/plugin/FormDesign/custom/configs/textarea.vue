c0ef52c280068d8e6800563d3d4888505486e89a
YanHeng <yanh@siactpower.com>
2022-08-06 14:55:34 +0800

    多行文本增加关联功能

--- src/plugin/FormDesign/custom/configs/textarea.vue
+++ src/plugin/FormDesign/custom/configs/textarea.vue
@@ -1,8 +1,5 @@
 <template>
   <div v-show="props.compType === 'textarea'">
-    <!-- <el-form-item label="字段名">
-      <el-input class="input" v-model="props"></el-input>
-    </el-form-item> -->
     <el-form-item label="字段">
       <el-tooltip class="item" effect="dark" content="请注意,ID的修改可能会导致该组件相关事件失效！" placement="left">
         <el-input class="input" v-model="props.id" @change="handlerChangeId"></el-input>
@@ -12,16 +9,13 @@
       <el-input class="input" v-model="props.label"></el-input>
     </el-form-item>
     <el-form-item label="提示符">
-      <el-input class="input" v-model="props.placeholder" placeholder="请输入提示符"/>
+      <el-input class="input" v-model="props.placeholder" placeholder="请输入提示符" />
     </el-form-item>
-    <!-- <el-form-item label="表单栅格">
-      <el-slider class="input" v-model="props.span" :max="24" :min="1" :marks="{12:''}"></el-slider>
-    </el-form-item> -->
     <el-form-item label="栅格间隔">
-      <el-input-number v-model="props.gutter"  :min="0"></el-input-number>
+      <el-input-number v-model="props.gutter" :min="0"></el-input-number>
     </el-form-item>
     <el-form-item label="标签宽度">
-      <el-input-number v-model="props.labelWidth"  :min="1" :max="200"></el-input-number>
+      <el-input-number v-model="props.labelWidth" :min="1" :max="200"></el-input-number>
     </el-form-item>
     <el-form-item label="显示标签">
       <el-switch v-model="props.showLabel" @change="handlerChangeLabel"></el-switch>
@@ -30,10 +24,10 @@
       <el-switch v-model="props.required"></el-switch>
     </el-form-item>
     <el-form-item label="输入框行数">
-      <el-input-number v-model="props.rows"  :min="0"></el-input-number>
+      <el-input-number v-model="props.rows" :min="0"></el-input-number>
     </el-form-item>
     <el-form-item label="长度限制">
-      <el-input-number v-model="props.maxlength" placeholder="字符长度"/>
+      <el-input-number v-model="props.maxlength" placeholder="字符长度" />
     </el-form-item>
     <el-form-item label="输入统计">
       <el-switch v-model="props['show-word-limit']"></el-switch>
@@ -47,40 +41,36 @@
     <el-form-item label="默认值">
       <el-input class="input" v-model="props.value"></el-input>
     </el-form-item>
-    <el-form-item label="关联" >
-      <el-input v-model="props.relationField" placeholder="#本表单取, $跨任务取值"></el-input>
-    </el-form-item>
+    <depend-value :currentField="props" :fieldOverviews="fieldOverviews" @dependChange="onDependChange" />
   </div>
 </template>
 <script>
-import {changeId} from '../mixin'
+import { changeId } from '../mixin'
+import DependValue from './component/DependValue.vue'
+
 export default {
-  name:"inputConfig",
-  props:['props','getFormId'],
-  components: {
-  },
-  mixins:[changeId],
-  data(){
-    return {
-    }
-  },
-  methods:{
-    handlerChangeLabel(val){
-      this.props.labelWidth = val?'80':'1'
+  name: 'inputConfig',
+  props: ['props', 'getFormId', 'fieldOverviews'],
+  components: { DependValue },
+  mixins: [changeId],
+  methods: {
+    onDependChange(dependValue) {
+      this.props.dependValue = dependValue
     },
-    handlerChangeDisStatus(val){
+    handlerChangeLabel(val) {
+      this.props.labelWidth = val ? '80' : '1'
+    },
+    handlerChangeDisStatus(val) {
       this.props.readOnly = !val
     },
-    handlerChangeReadStatus(val){
+    handlerChangeReadStatus(val) {
       this.props.disabled = !val
-    }
+    },
   },
-  mounted(){
-  }
 }
 </script>
 <style scoped>
-.input{
-  width:75%
+.input {
+  width: 75%;
 }
 </style>
