84dcb29bf69469fab9a8b4feee04669c1110eafe
Yingshengyu <yingshengyu@siact.com>
2022-08-10 17:38:54 +0800

    fix the bug 1193

--- src/view/configuration/startItemConfig/startItemCon.vue
+++ src/view/configuration/startItemConfig/startItemCon.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <el-dialog title="自定义启动" :visible.sync="dialogVisible2" v-if="dialogVisible2" width="30%" append-to-body>
+    <el-dialog
+        title="自定义启动"
+        :visible.sync="dialogVisible2"
+        v-if="dialogVisible2"
+        width="30%"
+        append-to-body
+    >
       <div id="item">
         <p class="tit">自定义启动项</p>
-        <el-tag v-for="tag in tags" :key="tag.name" closable @close="handleClose(tag)" :type="tag.type">
+        <el-tag
+            v-for="tag in tags"
+            :key="tag.name"
+            closable
+            @close="handleClose(tag)"
+            :class="'bg'+tag.backGroundColorIndex"
+           >
           {{ tag.name }}
         </el-tag>
         <div style="display: flex; margin-top: 15px">
@@ -13,14 +25,20 @@
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button class="savebtn" type="primary" @click="saveTag">保 存</el-button>
-        <el-button class="cancelbtn" @click="dialogVisible2 = false">取 消</el-button>
+        <el-button class="cancelbtn" @click="dialogVisible2= false">取 消</el-button>
       </span>
     </el-dialog>
-    <el-dialog title="启动项配置" :visible.sync="dialogVisible" width="80%" fullscreen append-to-body>
+    <el-dialog
+        title="启动项配置"
+        :visible.sync="dialogVisible"
+        width="80%"
+        fullscreen
+        append-to-body
+    >
       <div class="start_container">
         <div class="start_left">
-          <el-tree @node-click="handleNodeClick" class="tree" default-expand-all :data="data" :props="defaultProps">
-          </el-tree>
+          <el-tree @node-click="handleNodeClick" class="tree" default-expand-all :data="data"
+                   :props="defaultProps"></el-tree>
         </div>
         <div class="start_right">
           <p>自定义启动项</p>
@@ -29,30 +47,54 @@
           <!--          <el-button @click="editTable" v-if="btnFlag && tableData.length" style="margin-left: 960px" type="primary"-->
           <!--                     v-role="{ id: 'StartItemConfigEdit', type: 'button', business: business }">编辑-->
           <!--          </el-button>-->
-          <el-table v-if="tableFlag && tableData.length > 0" :data="tableData" style="width: 100%">
-            <el-table-column align="center" prop="name" label="启动项" width="180">
+          <el-table
+              v-if="tableFlag && tableData.length > 0"
+              :data="tableData"
+              style="width: 100%">
+            <el-table-column
+                align="center"
+                prop="name"
+                label="启动项"
+                width="180">
             </el-table-column>
-            <el-table-column align="center" prop="" label="Code" width="180">
+            <el-table-column
+                align="center"
+                prop=""
+                label="Code"
+                width="180">
               <template slot-scope="scope">
-                <el-input @blur="codeChange(scope.row.code)" :disabled="scope.row.disabled" v-model="scope.row.code">
-                </el-input>
+                <el-input @blur="codeChange(scope.row.code)" :disabled="scope.row.disabled"
+                          v-model="scope.row.code"></el-input>
               </template>
             </el-table-column>
-            <el-table-column align="center" prop="" label="启动类型" width="240">
+            <el-table-column
+                align="center"
+                prop=""
+                label="启动类型"
+                width="240">
               <template slot-scope="scope">
-                <el-input v-show="scope.row.startType === '1'" :disabled="scope.row.disabled" v-model="scope.row.value">
-                </el-input>
+                <el-input v-show="scope.row.startType ==='1'" :disabled="scope.row.disabled"
+                          v-model="scope.row.value"></el-input>
                 <el-select v-show="scope.row.startType === '2'" :disabled="scope.row.disabled"
-                  v-model="scope.row.thirdInterfaceId">
-                  <el-option v-for="(item, index) in optionsList" :key="index" :label="item.name" :value="item.id">
-                  </el-option>
+                           v-model="scope.row.thirdInterfaceId">
+                  <el-option v-for="(item,index) in optionsList" :key="index" :label="item.name"
+                             :value="item.id"></el-option>
                 </el-select>
-                <el-date-picker v-show="scope.row.startType === '3'" :disabled="scope.row.disabled"
-                  v-model="scope.row.value" type="date" value-format="yyyy-MM-dd hh:mm:ss" placeholder="选择日期">
+                <el-date-picker
+                    v-show="scope.row.startType === '3'"
+                    :disabled="scope.row.disabled"
+                    v-model="scope.row.value"
+                    type="date"
+                    value-format="yyyy-MM-dd hh:mm:ss"
+                    placeholder="选择日期">
                 </el-date-picker>
               </template>
             </el-table-column>
-            <el-table-column align="center" prop="" label="启动类型选择" width="420">
+            <el-table-column
+                align="center"
+                prop=""
+                label="启动类型选择"
+                width="420">
               <template slot-scope="scope">
                 <el-radio @change="radioChange" :disabled="scope.row.disabled" v-model="scope.row.startType" label="1">
                   输入框
@@ -65,23 +107,35 @@
                 </el-radio>
               </template>
             </el-table-column>
-            <el-table-column align="center" prop="" label="配置" width="100">
+            <el-table-column
+                align="center"
+                prop=""
+                label="配置"
+                width="100">
               <template slot-scope="scope">
                 <el-checkbox :disabled="scope.row.disabled" v-model="scope.row.isSetting"></el-checkbox>
               </template>
             </el-table-column>
-            <el-table-column align="center" width="100" prop="" label="必填">
+            <el-table-column
+                align="center"
+                width="100"
+                prop=""
+                label="必填">
               <template slot-scope="scope">
-                <el-checkbox @change="write(scope.row)" :disabled="scope.row.disabled" v-model="scope.row.isRequired">
-                </el-checkbox>
+                <el-checkbox @change="write(scope.row)" :disabled="scope.row.disabled"
+                             v-model="scope.row.isRequired"></el-checkbox>
               </template>
             </el-table-column>
-            <el-table-column align="center" v-if="processFlag" prop="" label="操作">
+            <el-table-column
+                align="center"
+                v-if="processFlag"
+                prop=""
+                label="操作">
               <template slot-scope="scope">
                 <span @click='deleteRow(scope.row)' style="color: #1d89ff; cursor: pointer; margin-right: 5px;"
-                  v-role="{ id: 'StartItemConfigDelete', type: 'button', business: business }">删除</span>
+                      v-role="{ id: 'StartItemConfigDelete', type: 'button', business: business }">删除</span>
                 <span @click="editTable(scope.row, scope.row.btnTxt)" style="color: #1d89ff; cursor: pointer"
-                  v-if="btnFlag && tableData.length">{{ scope.row.btnTxt }}</span>
+                      v-if="btnFlag && tableData.length">{{ scope.row.btnTxt }}</span>
               </template>
             </el-table-column>
           </el-table>
@@ -106,7 +160,7 @@ import {
 
 import FormTypeEnum from "@/enum/FormTypeEnum";
 import StartItemEnum from "@/enum/StartItemEnum";
-import { mapState } from 'vuex'
+import {mapState} from 'vuex'
 
 export default {
   name: "startItemCon",
@@ -129,7 +183,6 @@ export default {
       inputValue: '',
       businessConfigId: '',
       tags: [],
-      color: ['', '#d4fdd9', '#ffe5af', 'warning', 'danger'],
       dialogVisible: false,
       dialogVisible2: false,
       data: [],
@@ -175,7 +228,7 @@ export default {
               type: 'success',
               message: res.result
             })
-          } else {
+          } else  {
             this.$message({
               type: 'success',
               message: '删除成功'
@@ -211,12 +264,12 @@ export default {
     formatData(data) {
       data.forEach(item => {
         item.isSetting ?
-          item.isSetting = StartItemEnum.SURE_SETTING
-          : item.isSetting = StartItemEnum.NOT_SETTING
+            item.isSetting = StartItemEnum.SURE_SETTING
+            : item.isSetting = StartItemEnum.NOT_SETTING
 
         item.isRequired ?
-          item.isRequired = StartItemEnum.SURE_REQUIRED
-          : item.isRequired = StartItemEnum.NOT_SETTING
+            item.isRequired = StartItemEnum.SURE_REQUIRED
+            : item.isRequired = StartItemEnum.NOT_SETTING
 
         item.startType = +item.startType
       })
@@ -301,12 +354,12 @@ export default {
       row.disabled = false
       if (txt === '保存') {
         row.isSetting ?
-          row.isSetting = StartItemEnum.SURE_SETTING
-          : row.isSetting = StartItemEnum.NOT_SETTING
+            row.isSetting = StartItemEnum.SURE_SETTING
+            : row.isSetting = StartItemEnum.NOT_SETTING
 
         row.isRequired ?
-          row.isRequired = StartItemEnum.SURE_REQUIRED
-          : row.isRequired = StartItemEnum.NOT_SETTING
+            row.isRequired = StartItemEnum.SURE_REQUIRED
+            : row.isRequired = StartItemEnum.NOT_SETTING
         if (row.code && (row.thirdInterfaceId || row.value)) {
           if (row.id) { // 走修改接口
             row.updateBy = this.userInfo.account
@@ -369,7 +422,7 @@ export default {
     handleInputConfirm() {
       let inputValue = this.inputValue;
       if (inputValue.length > 0 && inputValue.length <= 100) {
-        let index = parseInt(Math.random() * 5)
+        let index = parseInt(Math.random() * 4)
         if (inputValue) {
           if (this.checkRepeat(inputValue)) {
             this.$message({
@@ -378,9 +431,8 @@ export default {
             })
           } else {
             this.tags.push({
-
               isUse: 0,
-              "createBy": this.userInfo.account,
+              createBy: this.userInfo.account,
               updateBy: this.userInfo.account,
               businessConfigId: this.currentId,
               code: "",
@@ -390,7 +442,7 @@ export default {
               name: this.inputValue,
               isSetting: true,
               isRequired: true,
-              type: this.color[index],
+              backGroundColorIndex: index,
               startType: '1',
               disabled: true,
               btnTxt: '编辑'
@@ -491,6 +543,25 @@ export default {
   width: 300px;
   height: 600px;
   border: 1px solid rgb(204, 204, 204);
+}
+
+.bg0 {
+    background: #D4FDD9
+}
+
+.bg1 {
+  background: #D1EBFB
+}
+    
+.bg2 {
+ background: #FFE5AF
+}
+.bg4 {
+ background: #CCCCCC
+}
+
+.bg5 {
+ background: #D3ECEF
 }
 
 @include tree;
