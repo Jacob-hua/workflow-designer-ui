994b93de3a6b76980c18eda694c50c5daa770d0f
YanHeng <yanh@siactpower.com>
2022-08-10 11:15:21 +0800

    row新增字段名属性

--- src/plugin/FormDesign/custom/configs/input.vue
+++ src/plugin/FormDesign/custom/configs/input.vue
@@ -1,8 +1,5 @@
 <template>
   <div v-show="props.compType === 'input'">
-    <!-- <el-form-item label="字段名">
-      <el-input class="input" v-model="props"></el-input>
-    </el-form-item> -->
     <el-form-item label="字段">
       <el-tooltip class="item" effect="dark" content="请注意,ID的修改可能会导致该组件相关事件失效！" placement="left">
         <el-input class="input" v-model="props.id" @change="handlerChangeId"></el-input>
@@ -14,9 +11,6 @@
     <el-form-item label="栅格">
       <el-input-number v-model="props.span" :min="1" :max="24" />
     </el-form-item>
-    <!--    <el-form-item label="宽度">-->
-    <!--      <el-input-number v-model="props.width"  :min="1" :max="100"/>%-->
-    <!--    </el-form-item>-->
     <el-form-item label="提示符">
       <el-input class="input" v-model="props.placeholder" placeholder="请输入提示符" />
     </el-form-item>
