6c79e5132ed2683bc77077a619e35b804d168f85
YanHeng <yanh@siactpower.com>
2022-08-23 15:52:30 +0800

    text配置面板增加关联配置

--- src/plugin/FormDesign/custom/items/text.js
+++ src/plugin/FormDesign/custom/items/text.js
@@ -2,25 +2,22 @@
  * 文本
  */
 export let text = {
-    id:'',
-    _id:'',
-    compType: 'text',
-    ele: 'fancy-text',
-    compName:'文本',
-    compIcon:'text',
-    //展示表单的模式
-    viewType:'component',
-    config: true,
-    form:false,
-    show:true,
-    text:'文本',
-    align:'left',
-    color:'#fff',
-    size:'14',
-    bold:'400',
-    // 控件左侧label内容
-    showLabel:false,
-    labelWidth: '0',
-    relationField: '' // 文本框关联字段
-  }
-  
+  id: '',
+  _id: '',
+  compType: 'text',
+  ele: 'fancy-text',
+  compName: '文本',
+  compIcon: 'text',
+  //展示表单的模式
+  viewType: 'component',
+  config: true,
+  form: false,
+  show: true,
+  text: '文本',
+  align: 'left',
+  color: '#fff',
+  size: '14',
+  bold: '400',
+  // 控件左侧label内容
+  showLabel: false,
+}
