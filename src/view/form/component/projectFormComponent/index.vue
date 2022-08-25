78bf126e7a6f1778d6e263a9c0993f9effa2095b
zhu qingyun <zhuqy@siactpower.com>
2022-08-25 16:22:53 +0800

    refactor

--- src/view/form/component/projectFormComponent/index.vue
+++ src/view/form/component/projectFormComponent/index.vue
@@ -153,7 +153,7 @@ export default {
           {
             min: 1,
             max: 100,
-            message: "长度在 1 到 100 个字符",
+            message: "表单名称长度在 1 到 100 个字符",
             trigger: "blur",
           },
         ],
@@ -165,9 +165,9 @@ export default {
         name: [
           { required: true, message: "请输入表单名称", trigger: "blur" },
           {
-            min: 0,
+            min: 1,
             max: 100,
-            message: "长度在 0 到 100 个字符",
+            message: "表单名称长度在 1 到 100 个字符",
             trigger: "blur",
           },
         ],
