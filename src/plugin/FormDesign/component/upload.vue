6beb3bbb704f08ab8546e3fa403057ebe04a3b8e
guoyf <guoyf@siactpower.com>
2022-08-25 16:59:19 +0800

    附件上传 布局调整

--- src/plugin/FormDesign/component/upload.vue
+++ src/plugin/FormDesign/component/upload.vue
@@ -1,10 +1,19 @@
 <template>
   <el-form-item
-    :label="fieldInfo.showLabel ? fieldInfo.label : ''"
     :label-width="fieldInfo.labelWidth + 'px'"
     :prop="fieldInfo.valuePath"
     :rules="rules"
   >
+    <template slot="label">
+      <el-tooltip
+        class="item"
+        effect="light"
+        :content="fieldInfo.showLabel ? fieldInfo.label : ''"
+        placement="top-start"
+      >
+        <span>{{ fieldInfo.showLabel ? fieldInfo.label : "" }}</span>
+      </el-tooltip>
+    </template>
     <el-upload
       v-if="isShowTextUpload"
       class="upload-demo"
@@ -62,7 +71,10 @@
     </el-dialog>
     <ul v-if="isShowTextUpload">
       <li @mousemove="fileMove(file)" v-for="(file, key) in value" :key="key">
-        <long-text :content="file.name" />
+        <long-text
+          contentStyle="width: 200px;color: #fff"
+          :content="file.name"
+        />
         <p>
           <span
             @click="handlePreview(file)"
@@ -324,6 +336,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+/deep/ {
+  .el-form-item__content {
+    margin-left: 60px !important;
+  }
+}
+/deep/ .el-form-item__label {
+  width: 100px !important;
+  overflow: hidden;
+
+  white-space: nowrap;
+
+  text-overflow: ellipsis;
+
+  -o-text-overflow: ellipsis;
+}
 ul {
   li {
     height: 40px;
