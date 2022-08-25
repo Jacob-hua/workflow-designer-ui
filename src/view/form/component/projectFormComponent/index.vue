60abd76cf16695c0cc5e0c8369950cefa43feb7e
guoyf <guoyf@siactpower.com>
2022-08-25 17:56:22 +0800

    删除 表单管理 冗余代码

--- src/view/form/component/projectFormComponent/index.vue
+++ src/view/form/component/projectFormComponent/index.vue
@@ -368,55 +368,6 @@ export default {
         }
       });
     },
-    async init() {
-      const container = this.$refs.form;
-      this.formEditor = new FormEditor({
-        container: container,
-      });
-      this.formEditor.importSchema(this.schema);
-      this.formEditor.on("selection.changed", (item) => {
-        console.log("选中？", item);
-      });
-    },
-    save() {
-      let { href, filename } = this.setEncoded(
-        "form",
-        "test",
-        JSON.stringify(this.formEditor.saveSchema())
-      );
-      this.downloadFunc(href, filename);
-    },
-    downloadProcess() {},
-    // 根据所需类型进行转码并返回下载地址
-    setEncoded(type, filename = "diagram", data) {
-      const encodedData = encodeURIComponent(data);
-      return {
-        filename: `${filename}.${type}`,
-        href: `data:application/${
-          type === "svg" ? "text/xml" : "bpmn20-xml"
-        };charset=UTF-8,${encodedData}`,
-        data: data,
-      };
-    },
-    downloadFunc(href, filename) {
-      if (href && filename) {
-        let a = document.createElement("a");
-        a.download = filename; // 指定下载的文件名
-        a.href = href; //  URL对象
-        a.click(); // 模拟点击
-        URL.revokeObjectURL(a.href); // 释放URL对象
-      }
-    },
-    // 加载本地文件
-    importLocalFile() {
-      const that = this;
-      const file = this.$refs.refFile.files[0];
-      const reader = new FileReader();
-      reader.readAsText(file);
-      reader.onload = () => {
-        this.formEditor.importSchema(JSON.parse(reader.result));
-      };
-    },
   },
   components: {
     formbpmn,
