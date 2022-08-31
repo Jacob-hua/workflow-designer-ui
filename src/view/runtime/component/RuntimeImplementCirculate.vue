1e8d97d91f2bfbcaf6ac087efcd87b6149f278a6
guoyf <guoyf@siactpower.com>
2022-08-31 11:55:46 +0800

    fix: 代办传阅标签溢出

--- src/view/runtime/component/RuntimeImplementCirculate.vue
+++ src/view/runtime/component/RuntimeImplementCirculate.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <div class="container">
-      <div class="content" v-for="({ assignee, circulations, taskId }, index) in circulate" :key="index">
+      <div
+        class="content"
+        v-for="({ assignee, circulations, taskId }, index) in circulate"
+        :key="index"
+      >
         <div v-show="assignee">{{ assignee }}:</div>
-        <div class="info" v-if="assignee === userInfo.account && circulations[0].unitList.length > 0">
+        <div
+          class="info"
+          v-if="
+            assignee === userInfo.account && circulations[0].unitList.length > 0
+          "
+        >
           <div>
-            <div class="user" v-for="userName in circulations[0].unitList" :key="userName">
-              {{ userName }}
+            <div
+              class="user"
+              v-for="userName in circulations[0].unitList"
+              :key="userName"
+            >
+              <long-text contentStyle="width: 80px" :content="userName" />
             </div>
           </div>
-          <el-button @click="onEditCirculate(taskId, circulations)"> 编辑 </el-button>
+          <el-button @click="onEditCirculate(taskId, circulations)">
+            编辑
+          </el-button>
         </div>
         <div class="empty" v-else-if="circulations[0].unitList.length == 0">
           <div>暂无传阅</div>
-          <el-button @click="onAddCirculate(taskId)" v-if="assignee === userInfo.account"> 添加 </el-button>
+          <el-button
+            @click="onAddCirculate(taskId)"
+            v-if="assignee === userInfo.account"
+          >
+            添加
+          </el-button>
         </div>
       </div>
     </div>
@@ -27,13 +47,13 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import RuntimePeople from './RuntimePeople.vue'
-import { getCirculation } from '@/api/unit/api.js'
-
+import { mapState } from "vuex";
+import RuntimePeople from "./RuntimePeople.vue";
+import { getCirculation } from "@/api/unit/api.js";
+import LongText from "@/component/LongText";
 export default {
-  name: 'RuntimeImplementCirculate',
-  components: { RuntimePeople },
+  name: "RuntimeImplementCirculate",
+  components: { RuntimePeople, LongText },
   props: {
     workflow: {
       type: Object,
@@ -42,61 +62,61 @@ export default {
   },
   data() {
     return {
-      editTaskId: '',
+      editTaskId: "",
       runtimePeopleVisible: false,
       runtimePeopleSelected: [],
-    }
+    };
   },
   computed: {
-    ...mapState('account', ['userInfo']),
+    ...mapState("account", ["userInfo"]),
     circulate() {
-      return this.workflow.curTrack?.circulationList ?? []
+      return this.workflow.curTrack?.circulationList ?? [];
     },
   },
   methods: {
     onAddCirculate(taskId) {
-      this.editTaskId = taskId
-      this.runtimePeopleVisible = true
+      this.editTaskId = taskId;
+      this.runtimePeopleVisible = true;
     },
     onEditCirculate(taskId, circulations) {
-      this.editTaskId = taskId
+      this.editTaskId = taskId;
       this.runtimePeopleSelected = circulations[0].unitList.map((userName) => ({
         userId: userName,
-      }))
-      this.runtimePeopleVisible = true
+      }));
+      this.runtimePeopleVisible = true;
     },
     async onRuntimePeopleSubmit({ addeds, removeds }) {
       if (removeds.length) {
-        let strDelete = removeds.map(({ userId }) => userId).join(',')
+        let strDelete = removeds.map(({ userId }) => userId).join(",");
         await getCirculation({
           unitList: strDelete,
-          operateType: 'delete',
+          operateType: "delete",
           taskId: this.editTaskId,
-          type: 'user',
+          type: "user",
           operator: this.userInfo.account,
           processInstanceId: this.workflow.processInstanceId,
-        })
+        });
       }
       if (addeds.length) {
-        let strData = addeds.map(({ userId }) => userId).join(',')
+        let strData = addeds.map(({ userId }) => userId).join(",");
         await getCirculation({
           unitList: strData,
-          operateType: 'add',
+          operateType: "add",
           taskId: this.editTaskId,
-          type: 'user',
+          type: "user",
           operator: this.userInfo.account,
           processInstanceId: this.workflow.processInstanceId,
-        })
+        });
       }
-      this.$message.success('传阅成功')
-      this.$emit('completed')
+      this.$message.success("传阅成功");
+      this.$emit("completed");
     },
   },
-}
+};
 </script>
 
 <style scoped lang="scss">
-@import '../index.scss';
+@import "../index.scss";
 
 @include container;
 
