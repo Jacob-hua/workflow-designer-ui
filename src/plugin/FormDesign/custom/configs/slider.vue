a2e94ea3cb7bc1b4d03500927239050adb7fd527
guoyf <guoyf@siactpower.com>
2022-09-08 14:55:10 +0800

    fix: 1、删除了组件，后面的属性信息还在 2、计数器组件有点点不合理 3、滑块范围选择后，字被盖住了

--- src/plugin/FormDesign/custom/configs/slider.vue
+++ src/plugin/FormDesign/custom/configs/slider.vue
@@ -35,7 +35,11 @@
     </el-form-item>
 
     <el-form-item label="最小值">
-      <el-input-number v-model="props.min" :min="0"></el-input-number>
+      <el-input-number
+        @change="minChange"
+        v-model="props.min"
+        :min="0"
+      ></el-input-number>
     </el-form-item>
     <el-form-item label="最大值">
       <el-input-number v-model="props.max" placeholder="字符长度" />
@@ -55,9 +59,17 @@
     <el-form-item label="禁用">
       <el-switch v-model="props.disabled"></el-switch>
     </el-form-item>
-    <el-form-item label="默认值">
+    <el-form-item v-if="!props.range" label="默认值">
       <el-input class="input" v-model="props.value"></el-input>
     </el-form-item>
+    <template v-else>
+      <el-form-item label="默认起始值">
+        <el-input class="input" v-model="defaultValue[0]"></el-input>
+      </el-form-item>
+      <el-form-item label="默认值终止值">
+        <el-input class="input" v-model="defaultValue[1]"></el-input>
+      </el-form-item>
+    </template>
   </div>
 </template>
 <script>
@@ -68,14 +80,33 @@ export default {
   components: {},
   mixins: [changeId],
   data() {
-    return {};
+    return {
+      defaultValue: [],
+      isUpdate: false,
+    };
+  },
+  watch: {
+    defaultValue(value) {
+      this.checkDefaultValue(value, this.isUpdate);
+    },
   },
   methods: {
+    checkDefaultValue(defaultValue) {
+      if (defaultValue[0] < this.props.min || defaultValue[0] > this.props.max)
+        defaultValue[0] = this.props.min;
+      if (defaultValue[1] > this.props.max) defaultValue[1] = this.props.max;
+      this.props.value = defaultValue;
+    },
+    minChange(minValue) {
+      if (this.props.range) {
+        this.defaultValue[0] = minValue;
+      }
+    },
     rangeChange(isRange) {
       if (isRange) {
-        this.props.value = [this.props.min, this.props.max];
+        this.defaultValue = [this.props.min, this.props.max];
       } else {
-        this.props.value = this.props.min;
+        this.defaultValue = this.props.min;
       }
     },
   },
