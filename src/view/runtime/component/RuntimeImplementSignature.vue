afe91e310543b23bb0bc991b856f06b3fb7a2830
guoyf <guoyf@siactpower.com>
2022-09-15 08:53:30 +0800

    fix: 工作流执行加减签名字超出长度修复

--- src/view/runtime/component/RuntimeImplementSignature.vue
+++ src/view/runtime/component/RuntimeImplementSignature.vue
@@ -5,8 +5,12 @@
         <div>会签:</div>
         <div class="info">
           <div>
-            <div class="user" v-for="({ userId }, index) in signature" :key="index">
-              {{ userId }}
+            <div
+              class="user"
+              v-for="({ userId }, index) in signature"
+              :key="index"
+            >
+              <long-text contentStyle="color: white" :content="userId" />
             </div>
           </div>
         </div>
@@ -23,12 +27,12 @@
 </template>
 
 <script>
-import RuntimePeople from './RuntimePeople.vue'
-import { postModifyProcessUser } from '@/api/unit/api.js'
-
+import RuntimePeople from "./RuntimePeople.vue";
+import { postModifyProcessUser } from "@/api/unit/api.js";
+import LongText from "@/component/LongText";
 export default {
-  components: { RuntimePeople },
-  name: 'RuntimeImplementSignature',
+  components: { RuntimePeople, LongText },
+  name: "RuntimeImplementSignature",
   props: {
     workflow: {
       type: Object,
@@ -39,36 +43,39 @@ export default {
     return {
       runtimePeopleVisible: false,
       runtimePeopleSelected: [],
-    }
+    };
   },
   computed: {
     signature() {
-      const assignees = this.workflow.curTrack?.assignee.split(',') ?? []
-      return assignees.reduce((signature, assignee) => [...signature, { userId: assignee }], [])
+      const assignees = this.workflow.curTrack?.assignee.split(",") ?? [];
+      return assignees.reduce(
+        (signature, assignee) => [...signature, { userId: assignee }],
+        []
+      );
     },
   },
   methods: {
     onEditSignature() {
-      this.runtimePeopleSelected = this.signature
-      this.runtimePeopleVisible = true
+      this.runtimePeopleSelected = this.signature;
+      this.runtimePeopleVisible = true;
     },
     async onRuntimePeopleSubmit({ selections }) {
-      const userList = selections.map(({ userId }) => userId)
+      const userList = selections.map(({ userId }) => userId);
       await postModifyProcessUser({
         processInstanceId: this.workflow.processInstanceId,
         taskKey: this.workflow.taskKey,
         taskId: this.workflow.newTaskId,
         userList,
-      })
-      this.$message.success('加减签成功')
-      this.$emit('completed')
+      });
+      this.$message.success("加减签成功");
+      this.$emit("completed");
     },
   },
-}
+};
 </script>
 
 <style scoped lang="scss">
-@import '../index.scss';
+@import "../index.scss";
 
 @include container;
 
