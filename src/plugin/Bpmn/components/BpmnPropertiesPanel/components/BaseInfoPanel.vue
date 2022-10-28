ad22fbd531fec24f4322a044518ef87bd9265bd7
yanheng <yanh@siactpower.com>
2022-10-28 18:31:10 +0800

    feat: 补充事件切换时需要过滤的类型

--- src/plugin/Bpmn/components/BpmnPropertiesPanel/components/BaseInfoPanel.vue
+++ src/plugin/Bpmn/components/BpmnPropertiesPanel/components/BaseInfoPanel.vue
@@ -62,8 +62,10 @@ export default {
         class: '',
       }
       if ([shapeType.TIMER_START_EVENT].includes(value)) {
+        this.clearListener()
         listener.class = 'com.siact.product.jwp.listener.ScheduleStartListener'
       } else if ([shapeType.TIMER_NON_INTERRUPTING_BOUNDARY_EVENT].includes(value)) {
+        this.clearListener()
         listener.class = 'com.siact.product.jwp.listener.TimeOutListener'
       } else if (
         [
@@ -74,6 +76,18 @@ export default {
           shapeType.END_EVENT, // 结束事件
           shapeType.INTERMEDIATE_THROW_EVENT, // 中间抛出事件
           shapeType.INTERMEDIATE_CATCH_EVENT, // 中间捕获事件
+          shapeType.BOUNDARY_EVENT, // 边界事件
+          shapeType.MESSAGE_BOUNDARY_EVENT, // 消息边界事件
+          shapeType.MESSAGE_NON_INTERRUPTING_BOUNDARY_EVENT, // 消息边界事件（非中断）
+          shapeType.TIMER_BOUNDARY_EVENT, // 定时边界事件
+          shapeType.ESCALATION_BOUNDARY_EVENT, // 升级边界事件
+          shapeType.ESCALATION_NON_INTERRUPTING_BOUNDARY_EVENT, // 升级边界事件（非中断）
+          shapeType.CONDITIONAL_BOUNDARY_EVENT, // 条件边界事件
+          shapeType.CONDITIONAL_NON_INTERRUPTING_BOUNDARY_EVENT, // 条件边界事件（非中断）
+          shapeType.ERROR_BOUNDARY_EVENT, // 错误边界事件
+          shapeType.SIGNAL_BOUNDARY_EVENT, // 信号边界事件
+          shapeType.SIGNAL_NON_INTERRUPTING_BOUNDARY_EVENT, // 信号边界事件（非中断）
+          shapeType.COMPENSATE_BOUNDARY_EVENT, // 补偿边界事件
         ].includes(value)
       ) {
         this.clearListener()
