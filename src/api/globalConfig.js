04ad04e9eed66b8e163472653d6d65190a786da6
yanheng <yanheng@siactpower.com>
2023-02-10 11:47:33 +0800

    feat: 启动项改造

--- src/api/globalConfig.js
+++ src/api/globalConfig.js
@@ -1,4 +1,4 @@
-import { get, post, deletefn, put } from "./unit/request";
+import { get, post, deletefn, put } from './unit/request'
 
 // api 分页列表
 // export const GetGlobalList = async (params) => {
@@ -10,120 +10,102 @@ import { get, post, deletefn, put } from "./unit/request";
 //       }
 //     }
 
-export const thirdAuth = (params) => post(`/token/thirdAuth`, params);
+export const thirdAuth = (params) => post(`/token/thirdAuth`, params)
 
-export const processVariable = (params) =>
-  get(`/execute/processVariable`, params);
+export const processVariable = (params) => get(`/execute/processVariable`, params)
 
 export const businessConfigWithTreeCreate = (params) =>
-  post("/config/businessConfig/businessConfigWithTree/create", params);
+  post('/config/businessConfig/businessConfigWithTree/create', params)
 
 // 获取 节点字典
 
-export const getAllBusinessConfig = (params) =>
-  get(`/config/businessConfig/getAllBusinessConfig`, params);
+export const getAllBusinessConfig = (params) => get(`/config/businessConfig/getAllBusinessConfig`, params)
 // 清除缓存的数据
 export const clearRedisBusinessConfigCode = (params) =>
-  get(`/config/businessConfig/clearRedisBusinessConfigCode`, params);
+  get(`/config/businessConfig/clearRedisBusinessConfigCode`, params)
 
 // 校验业务code
-export const checkCode = (params) =>
-  get(`/config/businessConfig/checkBusinessConfigCode`, params);
+export const checkCode = (params) => get(`/config/businessConfig/checkBusinessConfigCode`, params)
 
 // 获取项目列表及子业务级联
 
-export const getProjectList = (params) =>
-  post("/config/businessConfig/projectList", params);
+export const getProjectList = (params) => post('/config/businessConfig/projectList', params)
 
 // api 分页列表
-export const GetGlobalList = (params) => post("/config/global/list", params);
+export const GetGlobalList = (params) => post('/config/global/list', params)
 
 // api新增
-export const postSaveOrEdite = (params) =>
-  post("/config/global/create", params);
+export const postSaveOrEdite = (params) => post('/config/global/create', params)
 // api修改
-export const putSaveOrEdite = (params) => put("/config/global/update", params);
+export const putSaveOrEdite = (params) => put('/config/global/update', params)
 
 // 获取api 类型
-export const apiTypeList = (params) => get(`config/global/apiTypeList`, params);
+export const apiTypeList = (params) => get(`config/global/apiTypeList`, params)
 
 // 获取详情
-export const apiDetail = (params) => get(`/config/global/bean`, params);
+export const apiDetail = (params) => get(`/config/global/bean`, params)
 
 // 调用配置处理
-export const executeApi = (params) => post("/config/global/executeApi", params);
+export const executeApi = (params) => post('/config/global/executeApi', params)
 
 // 访问配置删除
 // 删除表单
-export const deleteApi = (id) => deletefn("/config/global/delete", { id: id });
+export const deleteApi = (id) => deletefn('/config/global/delete', { id: id })
 
-export const simulationRequest = (params) =>
-  post("/config/global/simulationRequest", params);
+export const simulationRequest = (params) => post('/config/global/simulationRequest', params)
 
 // // 业务配置 list
 
 export const getBusinessConfigBasicList = (params) =>
-  get(`/config/businessConfig/getBusinessConfigBasicList?tenantId=${params}`);
+  get(`/config/businessConfig/getBusinessConfigBasicList?tenantId=${params}`)
 
-export const getDicDataByClassify = () =>
-  get(`/config/dicData/getDicDataByClassify?classify=sys`);
+export const getDicDataByClassify = () => get(`/config/dicData/getDicDataByClassify?classify=sys`)
 
 export const checkBusinessConfig = (params) =>
   // get(`/config/businessConfig/checkBusinessConfig?type=${params.type}&name=${params.name}`);
-  get("/config/businessConfig/checkBusinessConfig", params);
+  get('/config/businessConfig/checkBusinessConfig', params)
 
 // export const addBusinessConfig = (params) =>
 //     post('/config/businessConfig/saveBusinessConfigWithTree', params);
 
 // 新增项目资源树
-export const createBusinessConfig = (params) =>
-  post("/config/businessConfig/businessConfigWithTree/create", params);
+export const createBusinessConfig = (params) => post('/config/businessConfig/businessConfigWithTree/create', params)
 
 // 修改项目资源树
-export const updateBusinessConfig = (params) =>
-  put("/config/businessConfig/businessConfigWithTree/update", params);
+export const updateBusinessConfig = (params) => put('/config/businessConfig/businessConfigWithTree/update', params)
 
 // 查询配置树状结构
 export const getBusinessConfigWithTree = (id, tenantId) =>
-  get(
-    `/config/businessConfig/getBusinessConfigWithTree?tenantId=${tenantId}&id=${id}`
-  );
+  get(`/config/businessConfig/getBusinessConfigWithTree?tenantId=${tenantId}&id=${id}`)
 // 删除配置
-export const deleteBusinessConfig = (params) =>
-  deletefn(`/config/businessConfig/delete`, params);
+export const deleteBusinessConfig = (params) => deletefn(`/config/businessConfig/delete`, params)
 
 // export const UpdatebusinessConfig = (params) => put(`/config/businessConfig`, params);
 // export const UpdatebusinessConfig = (params) => put(`/config/businessConfig/businessConfigWithTree/create`, params);
 
 // 启动项删除
-export const startConfigDelete = (params) =>
-  deletefn("/config/startConfig/delete", params);
+export const startConfigDelete = (params) => deletefn('/config/startConfig/delete', params)
 // 启动项修改/config/startConfig/update
-export const startConfigUpdate = (params) =>
-  put("/config/startConfig/update", params);
+export const startConfigUpdate = (params) => put('/config/startConfig/update', params)
 
 // 根据业务节点获取配置
-export const checkIsReferenced = (params) =>
-  get(`/config/startConfig/checkIsReferenced`, params);
+export const checkIsReferenced = (params) => get(`/config/startConfig/checkIsReferenced`, params)
 
-export const startConfig = (params) =>
-  post("/config/startConfig/create", params);
+export const startConfig = (params) => post('/config/startConfig/create', params)
 // 回去第三方杰奎
-export const getThirdInterfaceList = (params) =>
-  get(`/config/global/getThirdInterfaceList`, params);
+export const getThirdInterfaceList = (params) => get(`/config/global/getThirdInterfaceList`, params)
 //根据业务节点获取配置
 export const selectProcessStartConfigList = (id, tenantId) =>
-  get(
-    `/config/startConfig/selectProcessStartConfigList?tenantId=${tenantId}&businessConfigId=${id}`
-  );
+  get(`/config/startConfig/selectProcessStartConfigList?tenantId=${tenantId}&businessConfigId=${id}`)
 
-export const checkApiType = (params) =>
-  post(`/config/global/checkApiType`, params);
-export const checkSourceSort = (params) =>
-  post(`config/global/checkSourceSort`, params);
+export const checkApiType = (params) => post(`/config/global/checkApiType`, params)
+export const checkSourceSort = (params) => post(`config/global/checkSourceSort`, params)
 
 export const selectProcessStartConfigByCode = (params) =>
-  post(`/config/startConfig/selectProcessStartConfigByCode`, params);
+  post(`/config/startConfig/selectProcessStartConfigByCode`, params)
 
 // 获取全局唯一id
-export const getGlobalUUID = () => get(`/config/global/uuid`);
+export const getGlobalUUID = () => get(`/config/global/uuid`)
+
+// 获取选中节点表单
+export const getSelectProcessStartForm = (params) => post(` /config/startConfig/selectProcessStartForm`, params)
---
+++ src/view/configuration/startItemConfig/StartItemForm.vue
@@ -0,0 +1,275 @@
+<template>
+  <div>
+    <el-dialog
+      title="启动项配置"
+      :visible="visible"
+      :close-on-click-modal="false"
+      width="80%"
+      @close="onClose"
+      fullscreen
+      append-to-body
+    >
+      <div class="start_container">
+        <div class="start_left">
+          <PeTree
+            nodeKey="id"
+            labelName="name"
+            @nodeClick="handleNodeClick"
+            :data="businessData"
+            :props="peTreeProps"
+          ></PeTree>
+        </div>
+        <div class="start_right">
+          <div class="form-preview">
+            <div class="title">
+              <div>启动项表单</div>
+              <el-button class="remove-button" v-if="formShow"> 移除表单 </el-button>
+            </div>
+            <div class="content-wrapper form">
+              <div v-if="formShow">
+                <preview :itemList="formContent.fields" :formConf="formContent.config"></preview>
+              </div>
+              <div v-else class="empty-data">当前未关联表单</div>
+            </div>
+          </div>
+          <div class="form-list-wrapper">
+            <div class="title">表单筛选</div>
+            <div class="search-form">
+              <el-input v-model="formName" placeholder="请输入内容" @keyup.native.enter="fetchFormList"></el-input>
+            </div>
+            <div class="content-wrapper">
+              <div v-for="{ id, version, name, formContent: { list: fields, config }, docName } in formList" :key="id">
+                <div class="info">
+                  <div>{{ name }}</div>
+                  <div>
+                    {{ version }}
+                  </div>
+                </div>
+                <div class="operations">
+                  <el-popover placement="right" width="400" trigger="click">
+                    <preview :itemList="fields" :formConf="config"></preview>
+                    <span class="preview-button" slot="reference"> 查看 </span>
+                  </el-popover>
+                  <span class="link-button" @click="onLinked({ id, docName, fields, config })"> 关联 </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { getSelectProcessStartForm } from '@/api/globalConfig'
+import { designFormDesignServiceAll } from '@/api/unit/api.js'
+import preview from '@/plugin/FormDesign/component/preview'
+import { mapState } from 'vuex'
+import PeTree from '@/component/PeTree.vue'
+
+export default {
+  name: 'DeployOptions',
+  components: {
+    preview,
+    PeTree,
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    businessData: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+  },
+  data() {
+    return {
+      formContent: {},
+      formList: [],
+      formName: '',
+      tableData: [],
+      peTreeProps: {
+        children: 'children',
+        label: 'name',
+      },
+      activeBusiness: null,
+    }
+  },
+  computed: {
+    ...mapState('account', ['userInfo', 'tenantId']),
+    formShow() {
+      return Object.keys(this.formContent).length > 0
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit('update:visible', false)
+    },
+    handleNodeClick(data) {
+      if (data.parentId !== '-1') {
+        this.activeBusiness = data
+        this.fetchFormList()
+        this.fetchStartFrom()
+      }
+    },
+    onLinked({ fields, config }) {
+      this.formContent = {
+        fields,
+        config,
+      }
+    },
+    async fetchStartFrom() {
+      const { errorInfo, result } = await getSelectProcessStartForm({
+        businessConfigId: this.activeBusiness?.id,
+      })
+      if (errorInfo.errorCode) {
+        this.$message.error(errorInfo.errorMsg)
+        return
+      }
+      if (!result) {
+        this.formContent = {}
+        return
+      }
+      this.formContent = this.disableForm(result).formContent
+    },
+    async fetchFormList() {
+      const { errorInfo, result: formList = [] } = await designFormDesignServiceAll({
+        status: 'enabled',
+        tenantId: this.tenantId,
+        ascription: this.businessData[0]?.code,
+        business: this.activeBusiness?.code,
+        name: this.formName,
+      })
+      if (errorInfo.errorCode) {
+        this.$message.error(errorInfo.errorMsg)
+      }
+      this.formList = formList.map(this.disableForm)
+    },
+    disableForm(form) {
+      const newForm = { ...form }
+      newForm.formContent = JSON.parse(newForm.content)
+      newForm.formContent.fields = newForm.formContent.list
+      newForm.formContent.config && (newForm.formContent.config.disabled = true)
+      return newForm
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.start_container {
+  display: flex;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.start_right {
+  width: 80%;
+  padding: 10px;
+  margin-left: 20px;
+  border: 1px solid rgb(204, 204, 204);
+  display: flex;
+}
+
+.start_right button {
+  margin-top: 20px;
+}
+
+.start_left {
+  padding-left: 20px;
+  width: 300px;
+  border: 1px solid rgb(204, 204, 204);
+}
+
+.form-preview {
+  flex-grow: 1;
+  margin: 20px 0px 0px 20px;
+  display: flex;
+  flex-direction: column;
+  color: $font-color;
+
+  .title {
+    line-height: 40px;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .content-wrapper {
+    flex-grow: 1;
+    padding: 20px 30px;
+    overflow: auto;
+    border: 1px solid $border-color;
+  }
+}
+
+.remove-button {
+  @include resetBtn;
+}
+
+.form-list-wrapper {
+  width: 468px;
+  height: 680px;
+  padding: 0 38px;
+
+  .title {
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 400;
+  }
+
+  .search-form {
+    margin-top: 25px;
+  }
+
+  .content-wrapper {
+    height: 580px;
+    overflow: scroll;
+    margin: 10px 0;
+
+    & > div {
+      border: 1px solid $border-color;
+      background-color: $card-bg-color-1;
+      margin-top: 20px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      padding: 6px 20px;
+      font-size: 14px;
+
+      .info {
+        display: flex;
+        flex-direction: row;
+        color: #999999;
+
+        & > div:first-child {
+          width: 150px;
+        }
+      }
+
+      .operations {
+        display: flex;
+        flex-direction: row;
+      }
+    }
+  }
+}
+
+.preview-button {
+  margin-right: 20px;
+  color: #0199f3;
+  cursor: pointer;
+}
+
+.link-button {
+  color: #0dd5ef;
+  cursor: pointer;
+}
+</style>
