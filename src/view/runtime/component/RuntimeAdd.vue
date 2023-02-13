39f1d1c05da6a8cc1e7de1f4a917211eed7103c1
yanheng <yanheng@siactpower.com>
2023-02-13 15:31:57 +0800

    fix: 修复创建工单时初次选择不是首页，再选其他标签时报错

--- src/view/runtime/component/RuntimeAdd.vue
+++ src/view/runtime/component/RuntimeAdd.vue
@@ -117,6 +117,7 @@ export default {
   methods: {
     handleNodeClick(data) {
       this.getData.type = data.value
+      this.getData.page = 1
       this.getProcessList()
     },
     onCreateTicketVisible() {
