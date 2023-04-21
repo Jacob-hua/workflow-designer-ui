548c654c887b492fca4152860ab729cb8182e8d6
yanheng <yanheng@siactpower.com>
2023-04-21 14:37:40 +0800

    feat: 修改第三方登录

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -34,12 +34,10 @@
 </template>
 
 <script>
-import CONSTANT from '@/constant'
 import { userLogin } from '@/api/unit/api.js'
 import { getAllBusinessConfig, thirdAuth } from '@/api/globalConfig.js'
 import { mapMutations } from 'vuex'
 import { mapState } from 'vuex'
-import Cookies from 'js-cookie'
 
 export default {
   name: 'LoginPage',
@@ -54,7 +52,8 @@ export default {
     ...mapState('account', ['tenantId']),
   },
   created() {
-    if (Cookies.get('userInfo')) {
+    const userInfoString = sessionStorage.getItem('userInfo')
+    if (userInfoString && JSON.parse(userInfoString)) {
       this.thirdLogin()
     } else {
       sessionStorage.clear()
@@ -76,25 +75,33 @@ export default {
       })
     },
     thirdLogin() {
-      if (!Cookies.get('userInfo')) {
+      const userInfoString = sessionStorage.getItem('userInfo')
+
+      if (!userInfoString) {
         this.$message.error('登录失败')
         return
       }
-      const userInfo = JSON.parse(Cookies.get('userInfo'))
-      thirdAuth({
-        account: userInfo.account,
-        thirdToken: userInfo.mark,
-      }).then((res) => {
-        res.result.account = userInfo.account
-        res.result.name = userInfo.account
-        sessionStorage.setItem('loginData', JSON.stringify(res.result))
-        res.result.tenants[0] && this.updateTenantId(res.result.tenants[0].id)
-        this.getMapping(res.result.tenants[0]?.id)
-        this.$router.push('/home')
-        this.updateThirdLogin({ thirdLogin: true })
-      }).catch(() => {
-        this.$router.push('/home/noPermission')
-      })
+      try {
+        const userInfo = JSON.parse(userInfoString).accountInfo
+        thirdAuth({
+          account: userInfo.account,
+          thirdToken: userInfo.mark,
+        })
+          .then((res) => {
+            res.result.account = userInfo.account
+            res.result.name = userInfo.account
+            sessionStorage.setItem('loginData', JSON.stringify(res.result))
+            res.result.tenants[0] && this.updateTenantId(res.result.tenants[0].id)
+            this.getMapping(res.result.tenants[0]?.id)
+            this.$router.push('/home')
+            this.updateThirdLogin({ thirdLogin: true })
+          })
+          .catch(() => {
+            this.$router.push('/home/noPermission')
+          })
+      } catch (error) {
+        this.$message.error('登录失败')
+      }
     },
     getMapping(tenantId) {
       getAllBusinessConfig({
