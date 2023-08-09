e441e3e12a187ebf59e72ee789566565ec005963
liuhua <957404156@qq.com>
2023-08-09 17:29:00 +0800

    update: 历史流程导出

--- src/view/runtime/component/lookover.vue
+++ src/view/runtime/component/lookover.vue
@@ -446,22 +446,8 @@ export default {
         assignee: this.workflow.starterAssignee,
       }).then((res) => {
         console.log(res, "ddd");
-        // this.Download(res, 'test.pdf')
         downBold(`${this.workflow.workOrderName}`, 'xlsx', res)
       });
-    },
-    Download(content, filename) {
-      var eleLink = document.createElement("a");
-      eleLink.download = filename;
-      eleLink.style.display = "none";
-      // 字符内容转变成blob地址
-      var blob = new Blob([content]);
-      eleLink.href = URL.createObjectURL(blob);
-      // 触发点击
-      document.body.appendChild(eleLink);
-      eleLink.click();
-      // 然后移除
-      document.body.removeChild(eleLink);
     },
   },
 };
