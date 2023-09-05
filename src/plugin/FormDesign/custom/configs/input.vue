95c4868ee402bb86f776bb4ade6f48da1edc92b0
liuhua <957404156@qq.com>
2023-09-05 16:42:41 +0800

    fix: 输入框电话号码校验正则修改

--- src/plugin/FormDesign/custom/configs/input.vue
+++ src/plugin/FormDesign/custom/configs/input.vue
@@ -136,7 +136,7 @@ import iconDialog from "../../component/iconDialog";
 import DependValue from "./component/DependValue.vue";
 const defaultRules = {
   phone: {
-    rule: "0?(13|14|15|17|18|19)[0-9]{9}",
+    rule: "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$",
     msg: "您输入的电话号码不符合规则",
   },
   email: {
